0890d532b21515611e6461779973875be2951962
unknown <hninhnin0929>
2024-06-06 16:15:31 +0900

    nav links

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,7 +1,24 @@
+import Link from 'next/link';
 
+const links = [
+  { href: '/chat', label: 'chat' },
+  { href: '/tours', label: 'tours' },
+  { href: '/tours/new-tour', label: 'new tour' },
+  { href: '/profile', label: 'profile' },
+];
 
 export default function NavLinks() {
   return (
-    <div>NavLinks</div>
-  )
+    <ul className='menu text-base-content'>
+      {links.map((link) => {
+        return (
+          <li key={link.href}>
+            <Link href={link.href} className='capitalize'>
+              {link.label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
