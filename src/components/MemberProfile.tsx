e6ccdb8ea603a9ecd107d6b7002a5a4b9fcbb7fa
unknown <hninhnin0929>
2024-06-06 21:25:37 +0900

    theme toggle

--- src/components/MemberProfile.tsx
+++ src/components/MemberProfile.tsx
@@ -1,7 +1,17 @@
 
+import { UserButton } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function MemberProfile() {
+const MemberProfile = async () => {
+  const user = await currentUser();
+  const { userId } = auth();
+  // console.log(user);
   return (
-    <div>MemberProfile</div>
+    <div className="px-4 flex items-center gap-2">
+      <UserButton afterSignOutUrl="/" />
+      <p>{user?.emailAddresses[0].emailAddress}</p>
+    </div>
   )
 }
+
+export default MemberProfile
